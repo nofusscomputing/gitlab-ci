203a99903c28201f74304ca8689fae25a7a28aa0
Jon <jonathon.lockwood@networkedweb.com>
2023-05-28 16:24:12 +0930

    fix(sub_folder_changelog): dont create changelog for docs
    !34

--- sub-folder_changlog.sh
+++ sub-folder_changlog.sh
@@ -3,7 +3,7 @@
 for D in *; do
     if [ -d "${D}" ]; then
 
-      if [ "0${D}" != "0website-template" ]; then
+      if [ "0${D}" != "0website-template" ] && [ "0${D}" != "0docs" ]; then
         echo "[DEBUG] Creating changelog for sub-folder: ${D}"
 
         CHANGELOG_DATA=$(git log --pretty="format:%ci [%h](https://gitlab.com/nofusscomputing/projects/gitlab-ci/-/commit/%H) - %s  " --follow -- "${D}")
